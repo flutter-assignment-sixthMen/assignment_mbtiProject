e4660eeb4a897d66db05caba60e2e3cfeba99ba1
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-10-04 14:55:31 +0900

    mbti-data-jaehyun -> master merge 완료

--- lib/models/Question.dart
+++ lib/models/Question.dart
@@ -11,5 +11,6 @@ List<Question> questions = [
   Question(question: "파티에 가셨나요?", answers: [
     Answer(text: "낯선 사람을 포함한 많은 사람들과 소통", type: "E"),
     Answer(text: "이미 알고 있는 소수의 사람들과 소통하기", type: "I"),
+    //
   ]),
 ];
