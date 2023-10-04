b0300db50a7bd996534e58106919fadd93da7236
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-10-04 14:53:18 +0900

    master 에 merge

--- lib/models/Question.dart
+++ lib/models/Question.dart
@@ -7,11 +7,9 @@ class Question {
   Question({required this.question, required this.answers});
 }
 
-
 List<Question> questions = [
   Question(question: "파티에 가셨나요?", answers: [
     Answer(text: "낯선 사람을 포함한 많은 사람들과 소통", type: "E"),
     Answer(text: "이미 알고 있는 소수의 사람들과 소통하기", type: "I"),
-  ])
+  ]),
 ];
-
